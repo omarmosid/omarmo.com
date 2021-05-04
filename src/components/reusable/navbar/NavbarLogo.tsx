ea9d219975880a9dfbce06a373f3cb8df3cccb9d
Omar Mohammad <omar.mo.sid@gmail.com>
2021-05-04 06:06:13 +0530

    Fix logo

--- src/components/reusable/navbar/NavbarLogo.tsx
+++ src/components/reusable/navbar/NavbarLogo.tsx
@@ -1,14 +1,16 @@
+import { Image } from "@chakra-ui/image";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Link } from "gatsby";
 import React from "react";
+import Logo from "../../../images/logo.png"
 
 const NavbarLogo = () => {
   return (
     <>
       <Flex alignItems={["center", "flex-start"]}>
-        <Text fontSize="4xl" fontWeight="bold" textAlign={["left", "center"]}>
-          <Link to="/">Omar</Link>
-        </Text>
+        <Link to="/">
+          <Image src={Logo} height="50px" />
+        </Link>
       </Flex>
     </>
   );
